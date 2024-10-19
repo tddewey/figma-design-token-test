99a686a5902227f2c998976fc804a1b1aff5f3f6
Taylor Dewey <td@tddewey.com>
2024-10-18 18:00:19 -0700

    adding build process for csk

---
+++ csk/sd-build.js
@@ -0,0 +1,29 @@
+import { register } from '@tokens-studio/sd-transforms';
+import StyleDictionary from 'style-dictionary';
+
+// will register them on StyleDictionary object
+// that is installed as a dependency of this package.
+register(StyleDictionary);
+
+const sd = new StyleDictionary({
+  // make sure to have source match your token files!
+  // be careful about accidentally matching your package.json or similar files that are not tokens
+  source: ['csk/test.json'],
+  preprocessors: ['tokens-studio'], // <-- since 0.16.0 this must be explicit
+  platforms: {
+    css: {
+      transformGroup: 'tokens-studio', // <-- apply the tokens-studio transformGroup to apply all transforms
+      transforms: ['name/kebab'], // <-- add a token name transform for generating token names, default is camel
+      buildPath: 'csk/build/',
+      files: [
+        {
+          destination: 'token.css',
+          format: 'css/variables',
+        },
+      ],
+    },
+  },
+});
+
+await sd.cleanAllPlatforms();
+await sd.buildAllPlatforms();
