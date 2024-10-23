f2615a4cb027286869bb82de21041a111d87be7d
Taylor Dewey <td@tddewey.com>
2024-10-23 08:52:25 -0700

    PoC of splitting & merging outputs
    Splits output files, using a filter and assigns them different CSS selectors. So we can have :root and .button (in this example) Then an action to merge the two back together into a single file

--- csk/sd-build.js
+++ csk/sd-build.js
@@ -1,5 +1,11 @@
 import { register } from '@tokens-studio/sd-transforms';
 import StyleDictionary from 'style-dictionary';
+import fs from 'fs';
+
+/**
+ * Might be useful for if we need to generate css under a different selector from :root
+ * https://github.com/amzn/style-dictionary/issues/448
+ */
 
 // will register them on StyleDictionary object
 // that is installed as a dependency of this package.
@@ -14,7 +20,7 @@ const sd = new StyleDictionary({
   log: {"verbosity" : "verbose"},
   preprocessors: ['tokens-studio'], // <-- since 0.16.0 this must be explicit
   platforms: {
-    css: {
+    general: {
       transformGroup: 'tokens-studio', // <-- apply the tokens-studio transformGroup to apply all transforms
       transforms: ['name/kebab'], // <-- add a token name transform for generating token names, default is camel
       buildPath: 'csk/build/',
@@ -22,14 +28,56 @@ const sd = new StyleDictionary({
         {
           destination: 'token.css',
           format: 'css/variables',
+          filter: function(token){
+            return token.path.includes('base')
+          },
           options: {
             showFileHeader: true,
             outputReferences: true
           }
         },
+        {
+            destination: 'tokens-temp.css',
+            format: 'css/variables',
+            filter: function(token) {
+                return token.path.includes('button')
+            },
+            options: {
+                selector: ".button",
+                showFileHeader: true,
+                outputReferences: true
+            }
+        }
       ],
-    },
+      actions: ['mergeCssFiles']
+    }
   },
+});
+
+sd.registerAction({
+    name: 'mergeCssFiles',
+    do: function(dictionary, config){
+        console.log("merging temporary files")
+        const tempFile = config.buildPath + config.files[1].destination;
+        const destFile = config.buildPath + config.files[0].destination;
+        fs.appendFile(destFile, fs.readFileSync(tempFile), (err) => {
+            if (err) {
+              console.error('Error appending file:', err);
+            } else {
+              console.log('File appended successfully.');
+              
+              fs.unlink(tempFile, (err) => {
+                if (err) {
+                  console.error('Error deleting temporary file:', err);
+                  return;
+                }
+          
+                console.log('Temporary file deleted successfully.');
+              });
+
+            }
+          });          
+    }
 });
 
 await sd.cleanAllPlatforms();
